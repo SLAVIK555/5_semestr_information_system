8134e1c969db696f786b4cb4c9f783ff420de07f
SLAVIK555 <47494485+SLAVIK555@users.noreply.github.com>
2019-12-31 19:21:08 +0300

    Update allocation_and_send.h

--- allocation_and_send.h
+++ allocation_and_send.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef ALLOCATION_AND_SEND_H
 #define ALLOCATION_AND_SEND_H
 
@@ -6,26 +7,29 @@
 #include <conio.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/face.hpp>
 #include "opencv2/opencv.hpp"
-//#include <opencv2/face.hpp>
 //#include "drawLandmarks.hpp"
+
 
 using namespace std;
 using namespace cv;
-//using namespace cv::face;
+using namespace cv::face;
 
 class allocation_and_send {
-	Mat frame;
+	//Mat frame;
 	//Mat img;
 	//vector<string>allocated;
 public:
-	void allocattor(Mat frame, vector<string>allocated, vector<string>allocated_g, vector<string>landmarked, vector<Mat>m_allocated, vector<Mat>m_allocated_g);
-	void drawer(Mat frame, std::vector<Rect> faces);
-	void cropper(Mat image, Mat crop, std::vector<Rect> faces, vector<string>all, vector<Mat> img);
-	void name_sender(Mat image, vector<string> vec);
-	void img_sender(Mat image, vector<Mat> img);
-	void landmarker(Mat frame, std::vector<Rect> faces_g, vector<string>allocated_g, vector<string>landmarked, vector<Mat>m_allocated, vector<Mat>m_allocated_g);
+	int allocattor(Mat frame, Ptr<Facemark> facemark, int faceid, int& countt);
+	void drawer(Mat frame, std::vector<Rect> faces, int label, double confidence);
+	int cropper(Mat image, Mat crop, std::vector<Rect> faces, int faceid, int& countt/*, vector<string>all, vector<Mat> img*/);
+	//void name_sender(Mat image, vector<string> vec);
+	//void img_sender(Mat image, vector<Mat> img);
+	void landmarker(Mat img, std::vector<Rect> faces_g, Ptr<Facemark> facemark);
+	//void displayer(const string name, Mat showimg);
+	
 };
 
 #endif
-#pragma once
