a3aec792b645fa149a028df16abb105fb0aaa374
SLAVIK555 <47494485+SLAVIK555@users.noreply.github.com>
2019-12-31 19:17:53 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,92 @@
-#include <opencv/cv.h>
-#include "opencv2/objdetect/objdetect.hpp"
+#include <iostream>
+#include <conio.h>
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/opencv.hpp"
-#include <iostream>
-//#include "visitor.h"
-#include "allocation_and_send.h"
-//#include "identification.h"
-#include <vector>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/face.hpp"
 
+/*#include "highgui.hpp"
+#include "imgproc.hpp"
+#include "objdetect.hpp"
+#include "face.hpp"*/
+//#include "drawLandmarks.hpp"
+#include "allocation_and_send.h"
+#include "identification.h"
+//
 using namespace std;
 using namespace cv;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-vector<string>allocated;
-vector<string>allocated_g;
-vector<string>landmarked;
-vector<Mat>m_allocated;
-vector<Mat>m_allocated_g;
+using namespace cv::face;
 
-//void allocattor(Mat frame, vector<string> allocated) {};
+//vector<string>allocated;
+//vector<string>allocated_g;
+//vector<string>landmarked;
+//vector<Mat>m_allocated;
+//vector<Mat>m_allocated_g;
+//vector<Mat>m_landmarked;
+Mat frame;
+//Ptr<Facemark> facemark;
 
+int faceid;
+int countt=0;
 
-int main(void) {
-	allocation_and_send A;//object of class "allocation_and_send"
-						  // Read the image file
-	Mat frame = imread("pic.jpg");
-	// Apply the classifier to the frame
-	if (!frame.empty()) {
-		A.allocattor(frame, allocated, allocated_g, landmarked, m_allocated, m_allocated_g);
+int main(int argc, char** argv)
+{
+	cout << "enter user id" << endl;
+	cin >> faceid;
+	// Load Face Detector
+	//CascadeClassifier faceDetector("haarcascade_frontalface_alt2.xml");
+	// Create an instance of Facemark
+	Ptr<Facemark> facemark = FacemarkLBF::create();
+	// Load landmark detector
+	facemark->loadModel("lbfmodel.yaml");
+	// Set up webcam for video capture
+	VideoCapture cam(0);
+	// Variable to store a video frame and its grayscale 
+	//Mat frame, gray;
+	// Read a frame
+	identification I;
+	//I.trainer(faceid, countt);
+	while (cam.read(frame))
+	{
+		allocation_and_send A;//object of class "allocation_and_send"
+		A.allocattor(frame, facemark, faceid, countt);
+		//identification I;
+		//I.datasetter(frame, faceid, countt);
+		//cout << "11" << endl;
+		// Find face
+		//vector<Rect> faces;
+		// Convert frame to grayscale because
+		// faceDetector requires grayscale image.
+		//cvtColor(frame, gray, COLOR_BGR2GRAY);
+		// Detect faces
+		//faceDetector.detectMultiScale(gray, faces);
+		// Variable for landmarks. 
+		// Landmarks for one face is a vector of points
+		// There can be more than one face in the image. Hence, we 
+		// use a vector of vector of points. 
+		//vector< vector<Point2f> > landmarks;
+		// Run landmark detector
+		//bool success = facemark->fit(frame, faces, landmarks);
+		//if (success)
+		//{
+		// If successful, render the landmarks on the face
+		//for (size_t i = 0; i < faces.size(); i++)
+		//{
+		//	cv::rectangle(frame, faces[i], Scalar(0, 255, 0), 3);
+		//}
+		//for (int i = 0; i < landmarks.size(); i++)
+		//{
+		//	drawLandmarks(frame, landmarks[i]);
+		/*for (size_t j = 0; j < landmarks[i].size(); j++)
+		circle(frame, Point(landmarks[i][j].x, landmarks[i][j].y), 1, Scalar(255, 0, 0), 2);*/
+		//}
+		//}
+		// Display results 
+		//imshow("Facial Landmark Detection", frame);
+		// Exit loop if ESC is pressed
+		if (waitKey(1) == 27) break;
 	}
-	else {
-		printf(" --(!) No captured frame -- Break!");
-	}
-
-	waitKey(0);
-
-	system("pause");
+	//system("pause");
 	return 0;
 }
