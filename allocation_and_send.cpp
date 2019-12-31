416e72ae2337c605645405c83d5f76c706ad63c9
SLAVIK555 <47494485+SLAVIK555@users.noreply.github.com>
2019-12-31 19:19:29 +0300

    Update allocation_and_send.cpp

--- allocation_and_send.cpp
+++ allocation_and_send.cpp
@@ -1,25 +1,41 @@
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+//#include "catch.hpp"
 #define _CRT_SECURE_NO_WARNINGS
 #include <sstream>
 #include <iostream>
+#include <string>
 #include <conio.h>
 #include "opencv2/opencv.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include "allocation_and_send.h"
-//#include <opencv2/face.hpp>
-//#include "drawLandmarks.hpp"
+#include <opencv2/face.hpp>
+#include "drawLandmarks.hpp"
+#include "identification.h"
 
 
 using namespace std;
 using namespace cv;
-//using namespace cv::face;
+using namespace cv::face;
 
-void allocation_and_send::allocattor(Mat frame, vector<string> allocated, vector<string>allocated_g, vector<string>landmarked, vector<Mat>m_allocated, vector<Mat>m_allocated_g)
+// Create an instance of Facemark
+//Ptr<Facemark> facemark = FacemarkLBF::create();
+// Load landmark detector
+//facemark->loadModel("lbfmodel.yaml");
+
+int allocation_and_send::allocattor(Mat frame, Ptr<Facemark> facemark, int faceid, int& countt)
 {
+	identification I;
+
 	Mat crop;
 	Mat gray;
 
+
+	// Create an instance of Facemark
+	//Ptr<Facemark> facemark = FacemarkLBF::create();
+	// Load landmark detector
+	//facemark->loadModel("lbfmodel.yaml");
 	// Load Face cascade (.xml file)
 	CascadeClassifier faceDetector("haarcascade_frontalface_alt2.xml");
 
@@ -35,13 +51,17 @@ void allocation_and_send::allocattor(Mat frame, vector<string> allocated, vector
 	faceDetector.detectMultiScale(gray, faces_g);
 
 	//Calling methoods
-	drawer(frame, faces);
-	cropper(frame, crop, faces, allocated, m_allocated);
-	cropper(gray, crop, faces_g, allocated_g, m_allocated_g);
-	landmarker(frame, faces_g, allocated_g, landmarked, m_allocated, m_allocated_g);
+	//drawer(frame, faces);
+	//cropper(frame, crop, faces, allocated, m_allocated);
+	//cropper(gray, crop, faces_g, allocated_g, m_allocated_g);
+	//cropper(gray, crop, faces_g, faceid, countt/*, allocated_g, m_allocated_g*/);
+	//landmarker(frame, faces_g, allocated_g, landmarked, m_allocated_g, m_landmarked, facemark);
+	//I.datasetter(gray, faceid, countt);
+	I.predictor(frame, faces);
+	return countt;
 }
 
-void allocation_and_send::drawer(Mat frame, std::vector<Rect> faces)
+void allocation_and_send::drawer(Mat frame, std::vector<Rect> faces, int label, double confidence)
 {
 	// Draw rectangle on the detected faces
 	Mat drawres = frame;
@@ -49,10 +69,22 @@ void allocation_and_send::drawer(Mat frame, std::vector<Rect> faces)
 	{
 		rectangle(drawres, faces[i], Scalar(0, 255, 0), 3);
 	}
+
+	stringstream ss;
+	ss << label;
+	putText(drawres, ss.str(), Point(faces[0].x, faces[0].y), 1, 2, Scalar(0, 0, 255), 2);
+
+	string s = to_string(confidence);
+	putText(drawres, ss.str(), Point(faces[faces.size()].x, faces[faces.size()].y), 1, 2, Scalar(0, 0, 255), 2);
+
+	imshow("predict", drawres);
+	//const string rectangl;
+	//displayer(rectangl, drawres);
 }
 
-void allocation_and_send::cropper(Mat image, Mat crop, std::vector<Rect> faces, vector<string> all, vector<Mat> img)
+int allocation_and_send::cropper(Mat image, Mat crop, std::vector<Rect> faces, int faceid, int& countt/*, vector<string> all, vector<Mat> img*/)
 {
+	//identification I;
 	// Set Region of Interest
 	cv::Rect roi_b;
 	cv::Rect roi_c;
@@ -91,14 +123,22 @@ void allocation_and_send::cropper(Mat image, Mat crop, std::vector<Rect> faces, 
 
 		crop = image(roi_b);
 
-		name_sender(crop, all);
-		img_sender(crop, img);//img-vector
-		//sender(crop, all, img);
-
+		//name_sender(crop, all);
+		//img_sender(crop, img);//send to img-vector
+		//landmarker(crop, faces_g, allocated_g, landmarked, m_allocated_g, m_landmarked, facemark);
 	}
+	if (!crop.empty()) {
+		//I.datasetter(crop, faceid, countt);
+	}
+	//waitKey(0);
+	//const string rectangl;
+	//displayer(rectangl, crop);
+	//return crop;
+	cout << countt << endl;
+	//return counttt;
 }
 
-void allocation_and_send::name_sender(Mat image, vector<string> vec)
+/*void allocation_and_send::name_sender(Mat image, vector<string> vec)
 {
 	string filename;
 	int filenumber = 0;
@@ -112,64 +152,59 @@ void allocation_and_send::name_sender(Mat image, vector<string> vec)
 
 	//imwrite(filename, image);//inwrite image
 	vec.push_back(filename);//add to base of name of files
-}
-
-void allocation_and_send::img_sender(Mat image, vector<Mat> img)
-{
-	img.push_back(image);//add to base of picture
-}
-
-/**
-void allocation_and_send::sender(Mat image, vector<string> vec, vector<Mat> img)
-{
-	string filename;
-	int filenumber = 0;
-
-	// Form a filename
-	filename = "";
-	stringstream ssfn;
-	ssfn << filenumber << ".jpg";
-	filename = ssfn.str();
-	filenumber++;
-
-	imwrite(filename, image);//inwrite image
-	vec.push_back(filename);//add to base of name of files
-	//img.push_back(image);//add to base of picture
 }*/
 
-void allocation_and_send::landmarker(Mat frame, std::vector<Rect> faces_g, vector<string> allocated_g, vector<string> landmarked, vector<Mat>m_allocated, vector<Mat>m_allocated_g)
+/*void allocation_and_send::img_sender(Mat image, vector<Mat> img)
 {
+	img.push_back(image);//add to base of picture
+}*/
+
+void allocation_and_send::landmarker(Mat img, std::vector<Rect> faces_g, Ptr<Facemark> facemark)
+{
+	//identification I;
 	// Create an instance of Facemark
-	Ptr<Facemark> facemark = FacemarkLBF::create();
+	//Ptr<Facemark> facemark = FacemarkLBF::create();
 	// Load landmark detector
-	facemark->loadModel("lbfmodel.yaml");
+	//facemark->loadModel("lbfmodel.yaml");
 	// Create local copy of image
-	Mat img;
-	for (int it = 0; it<m_allocated_g.size(); it++) {
-		img = m_allocated_g[it];
+	//Mat img;
+	//for (int it = 0; it<m_allocated_g.size(); it++) {
+		//img = m_allocated_g[it];
 		// Variable for landmarks. 
 		// Landmarks for one face is a vector of points
 		// There can be more than one face in the image. Hence, we 
 		// use a vector of vector of points. 
 		vector< vector<Point2f> > landmarks;
 		// Run landmark detector
-		bool success = facemark->fit(frame, faces_g, landmarks);
+		bool success = facemark->fit(img, faces_g, landmarks);
 		if (success)
 		{
 			// If successful, render the landmarks on the face
 			for (size_t i = 0; i < faces_g.size(); i++)
 			{
-				cv::rectangle(frame, faces_g[i], Scalar(0, 255, 0), 3);
+				cv::rectangle(img, faces_g[i], Scalar(0, 255, 0), 3);
 			}
 			for (int i = 0; i < landmarks.size(); i++)
 			{
-				drawLandmarks(frame, landmarks[i]);
-				/*for (size_t j = 0; j < landmarks[i].size(); j++)
-				circle(frame, Point(landmarks[i][j].x, landmarks[i][j].y), 1, Scalar(255, 0, 0), 2);*/
+				drawLandmarks(img, landmarks[i]);
+				for (size_t j = 0; j < landmarks[i].size(); j++) {
+					circle(img, Point(landmarks[i][j].x, landmarks[i][j].y), 1, Scalar(0, 0, 0), 2);
+					//cout << 1 << endl;
+					//I.identificator(img, landmarks, i);
+				}
 			}
 		}
 		// Display results 
-		//imshow("Facial Landmark Detection", frame);
-		img_sender(img, landmarked);
-	}
+		//imshow("Facial Landmark Detection", img);
+		//img_sender(img, m_landmarked);
+		/////////////////////
+		//const string fm;
+		//displayer(fm, img);
+	//}
+
 }
+
+/*void allocation_and_send::displayer(const string name, Mat showimg)
+{
+	imshow(name, showimg);
+}*/
